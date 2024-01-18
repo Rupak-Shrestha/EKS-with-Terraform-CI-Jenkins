8a03472ff3ddb03b02d992ca51d434c445639f17
Rupak Shrestha <rupak.shrestha404@gmail.com>
2024-01-18 20:20:23 +0000

    cluster public access

--- eks/main.tf
+++ eks/main.tf
@@ -38,6 +38,8 @@ module "eks" {
   cluster_name    = "eks-cluster"
   cluster_version = "1.27"
 
+  cluster_endpoint_public_access = true
+  
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
